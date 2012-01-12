6c31ce3a079b73cbe410787e553fa66d9fd02167
Eugenio Piasini <e.piasini@ucl.ac.uk>
2012-01-12 11:54:58 +0000

    Added facilities for fitting a NeuroMLv1 blocking expression to Jason's woodhull fit to Takahashi1996.

--- waveforms.py
+++ waveforms.py
@@ -48,8 +48,9 @@ n_VK2off_J = -45.895
 pulse_duration = 2*as_tau_dec_3_J
 time_points = np.arange(0.01, pulse_duration, 0.001)
 
-ampa_fit=True
-nmda_fit=True
+ampa_fit=False
+nmda_fit=False
+p7_9_fit=True
 
 def peak_time(tau_rise, tau_dec):
     return (tau_dec*tau_rise)/(tau_dec-tau_rise)*log(tau_dec/tau_rise)
@@ -75,6 +76,32 @@ def n_block_eric(v, K1slope, K2slope, VK1on, VK1off, VK2off):
      K2OFF = exp( -( v - VK2off ) / K2slope )
      block = ( K1OFF + K2OFF ) / ( K1OFF + K2OFF + K1ON )
      return block
+
+def block_takahashi_1996(v):
+    z = 2
+    F = 96.4853415
+    R = 8.3144621
+    T = 24.5 + 273.15
+
+    S = (z * F * v) / (2 * R * T)
+
+    #mgOut = 1.5 * 1000 # Eric's extracellular concentration
+    mgOut = 1.0 * 1000 # matches Takahashi 1996 data
+
+
+    # Jason's fit to P08 data
+    k1on = 0.95334
+    k1off = 1876.33
+    k2off = 3.33674
+    Fbinding = 0.852014
+    Fpermeation = 0.966919
+
+    K1ON = mgOut * k1on * exp( -Fbinding * S )
+    K1OFF = k1off * exp( Fbinding * S )
+    K2OFF = k2off * exp( -Fpermeation * S )
+
+    block = ( K1OFF + K2OFF ) / ( K1OFF + K2OFF + K1ON )
+    return block
 
 def n_block(v, eta, gamma):
     return 1/(1 + eta * exp(-gamma*v))
@@ -167,6 +194,37 @@ if nmda_fit:
     nb_ax.set_ylabel('block factor')
     nb_ax.legend()
     nb_ax.grid()
+
+#===P7_9 blocking factor====
+if p7_9_fit:
+
+    lower_limit = -80
+    upper_limit = 0
+    plot_voltage_points = np.arange(-100, 40, 0.001) # plot region
+    voltage_points = np.arange(lower_limit, upper_limit, 0.001) # fit region
+    takahashi_block_values = np.array([block_takahashi_1996(v) for v in voltage_points])
+
+    p7_9_block_params_f = curve_fit(n_block, voltage_points, takahashi_block_values, [1, 0.0035])[0]
+    n_block_fitted = partial(n_block, eta=p7_9_block_params_f[0], gamma=p7_9_block_params_f[1])
+    n_block_values = np.array([n_block_fitted(v) for v in voltage_points])
+
+    p7_9_file = open('p7_9_fit.txt', 'w')
+    p7_9_file.write("NeuroMLv1 fit to Jason's Woodhull Fit of Takahashi 1996 results for p7-9:\n")
+    p7_9_file.write("Fitting region: from {lower}mV to {upper}mV\n".format(lower=lower_limit, upper=upper_limit))
+    p7_9_file.write("NMDA block expression: {params}\n".format(params=zip(['eta', 'gamma'], p7_9_block_params_f)))
+    p7_9_file.close()
+
+    p7_9_fig = plt.figure()
+    p7_9_ax = p7_9_fig.add_subplot(111)
+    p7_9_ax.plot(plot_voltage_points, [block_takahashi_1996(v) for v in plot_voltage_points], label="Jason's fit to Takahashi1996 p7-9")
+    p7_9_ax.plot(plot_voltage_points, [n_block_fitted(v) for v in plot_voltage_points], label="NeuroMLv1\n%s" % str(zip(['eta', 'gamma'], p7_9_block_params_f)))
+    p7_9_ax.set_title('NMDA block (fitted between %dmV and %dmV)' % (lower_limit, upper_limit))
+    p7_9_ax.set_xlabel('membrane potential (mV)')
+    p7_9_ax.set_ylabel('unblock factor')
+    p7_9_ax.legend(loc='upper left')
+    p7_9_ax.grid()
+    
+    
     
 param_file.close()
 plt.show()
