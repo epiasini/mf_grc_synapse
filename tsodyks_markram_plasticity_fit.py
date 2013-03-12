46db734517b15a1ef2fb326d4772a407c29631c9
Eugenio Piasini <e.piasini@ucl.ac.uk>
2013-03-12 18:01:41 +0000

    Start implementing fit to Jason's data for mf->grc synapse.
    Still not functional for the time being.

---
+++ tsodyks_markram_plasticity_fit.py
@@ -0,0 +1,136 @@
+import random
+import time
+import inspyred
+import numpy as np
+from matplotlib import pyplot as plt
+
+from waveforms import a_g_comp
+
+import pdb
+
+DATA_DIR = "/home/ucbtepi/doc/jason_data/JasonsIAFmodel/Jason_Laurence_AMPA_NMDA_Trains"
+NMDA_DIR = DATA_DIR + "/NMDA"
+AMPA_DIR = DATA_DIR + "/AMPA"
+TIME_DIR = DATA_DIR + "/StimTimes"
+PULSE_CUTOFF = 200
+
+
+class Rothman_AMPA_STP(inspyred.benchmarks.Benchmark):
+    def __init__(self):
+        # parameters: for both direct and spillover components, 7 for
+        # the waveform and 2 for the plasticity
+        inspyred.benchmarks.Benchmark.__init__(self, dimensions=18)
+        # load experimental data
+        self.frequencies = [5, 10, 30, 50, 80, 100, 150]
+        self.n_trials = 4
+        self.exp_data = []
+        self.exp_pulses = []
+        for freq in self.frequencies:
+            for trial in range(self.n_trials):
+                self.exp_pulses.append(np.loadtxt(TIME_DIR + "/gp{0}_{1}hz_times.txt".format(trial,
+                                                                                             freq)))
+                self.exp_data.append(np.loadtxt(AMPA_DIR + "/Avg_AMPA_{0}hz_G{1}.txt".format(freq,
+                                                                                             trial)))
+        # needed to generate random values for tau_rec in the _open_
+        # interval (0,2)
+        self.epsilon = 1e-15
+
+        self.bounds = [(0.2, 0.3), # d_tau_rise **direct (waveform)**
+                       (0.4, 0.5), # d_a1
+                       (0.06, 0.10), # d_a2
+                       (0.01, 0.02), # d_a3
+                       (0.2, 0.3), # d_tau_dec1
+                       (2., 3.), # d_tau_dec2
+                       (18., 25.), # d_tau_dec3
+                       (0.2, 1.), # d_u_se   **direct (STD)**
+                       (30., 100.), # d_tau_dep
+                       (0.6, 1.0), # s_tau_rise **spillover (waveform)
+                       (0.08, 0.10), # s_a1
+                       (0.05, 0.2), # s_a2
+                       (0.005, 0.010), # s_a3
+                       (0.5, 1.0), # s_tau_dec1
+                       (5., 10.), # s_tau_dec2
+                       (27., 42.), #s_tau_dec3
+                       (0.2, 1.), # s_u_se   **spillover (STD)**
+                       (30., 100.)] # s_tau_dep
+
+        self.bounder = inspyred.ec.Bounder([b[0] for b in self.bounds],
+                                           [b[1] for b in self.bounds])
+        self.maximize = False
+
+    def dep_table(self, pulse_train, u_se, tau_rec):
+        # r[n,k] is the remaining fraction of synaptic resource before
+        # nth pulse. The remaing fraction before the 0-th pulse is
+        # supposed to be 1.
+        r =  np.ones(shape=pulse_train.shape, dtype=np.float)
+        pulse_intervals = np.diff(pulse_train)
+        # evolve according to tsodyks-markram model
+        for n in range(1, r.shape[0]):
+            r[n] = 1 - np.exp(-(pulse_intervals[n-1])/tau_rec) * (1 - r[n-1]*(1-u_se))
+        # by the (facilitation-less) tsodyks-markram model, the
+        # plasticity factors are obtained by multiplying r by u_se (to
+        # give the actual fraction of synaptic resource utilised at
+        # any givn pulse) and by another constant representing the
+        # absolute synaptic efficacy (ie the response amplitude when
+        # all the synaptic resource is used). We omit this step, as we
+        # are only interested in relative scaling with respect to the
+        # first pulse.
+        return r
+
+    def synthetic_conductance_signal(self, time_points, pulse_train, tau_rise, a1, a2, a3, tau_dec1, tau_dec2, tau_dec3, u_se, tau_rec):
+        # this is meant to be used for a single component (direct or
+        # spillover)
+        dep_factors = self.dep_table(pulse_train, u_se, tau_rec)
+        single_pulse_waveform = np.array([a_g_comp(t, tau_rise, a1, tau_dec1, a2, tau_dec2, a3, tau_dec3) for t in time_points[0:np.searchsorted(time_points, PULSE_CUTOFF)]])
+        signal = np.zeros(shape=time_points.shape[0])
+        for n, pulse in enumerate(pulse_train):
+            print n, pulse
+            start = np.searchsorted(time_points, pulse)
+            signal[start:start+single_pulse_waveform.shape[0]] += single_pulse_waveform * dep_factors[n]
+        return signal
+
+    def fitness_to_experiment(self, cs):
+        distances = []
+        for k, ep in enumerate(self.exp_pulses):
+            signal_direct = self.synthetic_conductance_signal(self.exp_data[k][:,0], ep, *cs[:9])
+            signal_spillover = self.synthetic_conductance_signal(self.exp_data[k][:,0], ep, *cs[9:])
+            distance = np.linalg.norm(signal_direct+signal_spillover-self.exp_data[k][:,1])
+        return sum(distances)
+
+    def generator(self, random, args):
+        return [random.uniform(self.bounder.lower_bound[k],
+                               self.bounder.upper_bound[k]) for k in range(self.dimensions)]
+
+    def evaluator(self, candidates, args):
+        return [self.fitness_to_experiment(cs) for cs in candidates]
+
+
+def main():
+    #a = np.loadtxt(NMDA_DIR + "/NMDA_10hz_G2_181103.txt")
+    #a = np.loadtxt(AMPA_DIR + "/Avg_AMPA_10hz_G2.txt")
+    #t = np.loadtxt(TIME_DIR + "/gp2_10hz_times.txt")
+
+    #fig, ax = plt.subplots()
+    #ax.plot(a[:,0], a[:,1])
+    #ax.scatter(t, np.zeros(shape=t.shape)-0.05, color='r')
+    #plt.show()
+    prng = random.Random()
+    prng.seed(int(time.time()))
+
+    problem = Rothman_AMPA_STP()
+    algorithm = inspyred.swarm.PSO(prng)
+    algorithm.topology = inspyred.swarm.topologies.ring_topology
+    algorithm.terminator = inspyred.ec.terminators.evaluation_termination
+    final_pop = algorithm.evolve(evaluator=problem.evaluator,
+                                 generator=problem.generator,
+                                 maximize=problem.maximize,
+                                 bounder=problem.bounder,
+                                 max_evaluations=5000,
+                                 pop_size=100,
+                                 neighborhood_size=5)
+    # Sort and print the best individual
+    final_pop.sort(reverse=True)
+    print(final_pop[0])
+
+if __name__ == '__main__':
+    main()
--- waveforms.py
+++ waveforms.py
@@ -106,128 +106,129 @@ def block_takahashi_1996(v):
 def n_block(v, eta, gamma):
     return 1/(1 + eta * exp(-gamma*v))
 
-param_file = open('fitted_parameters.txt', 'w')
+if __name__ == '__main__':
+    param_file = open('fitted_parameters.txt', 'w')
 
-#====AMPA====
-if ampa_fit:
-    # prepare the two 'scaled' components (direct and s.over) to fit the multi_decay_syn expressions to 
-    ad_g_J_values = np.array([a_g_J_unnorm_comp(t, ad_N_J, ad_tau_rise_J, ad_a1_J, ad_tau_dec_1_J, ad_a2_J, ad_tau_dec_2_J, ad_a3_J, ad_tau_dec_3_J) for t in time_points])
-    as_g_J_values = np.array([a_g_J_unnorm_comp(t, as_N_J, as_tau_rise_J, as_a1_J, as_tau_dec_1_J, as_a2_J, as_tau_dec_2_J, as_a3_J, as_tau_dec_3_J) for t in time_points])
+    #====AMPA====
+    if ampa_fit:
+        # prepare the two 'scaled' components (direct and s.over) to fit the multi_decay_syn expressions to
+        ad_g_J_values = np.array([a_g_J_unnorm_comp(t, ad_N_J, ad_tau_rise_J, ad_a1_J, ad_tau_dec_1_J, ad_a2_J, ad_tau_dec_2_J, ad_a3_J, ad_tau_dec_3_J) for t in time_points])
+        as_g_J_values = np.array([a_g_J_unnorm_comp(t, as_N_J, as_tau_rise_J, as_a1_J, as_tau_dec_1_J, as_a2_J, as_tau_dec_2_J, as_a3_J, as_tau_dec_3_J) for t in time_points])
 
-    max_ad_g_J_values = ad_g_J_values.max()
-    max_as_g_J_values = as_g_J_values.max()
+        max_ad_g_J_values = ad_g_J_values.max()
+        max_as_g_J_values = as_g_J_values.max()
 
-    a_g_J_unnorm_values = ad_g_J_values/max_ad_g_J_values + a_sd_ratio_J*(as_g_J_values/max_as_g_J_values)
-    max_a_g_J_unnorm_values = a_g_J_unnorm_values.max()
-    a_g_J_values_norm = a_g_J_unnorm_values/max_a_g_J_unnorm_values
-    a_g_J_values_scaled_to_max = a_g_peak_J*a_g_J_values_norm
+        a_g_J_unnorm_values = ad_g_J_values/max_ad_g_J_values + a_sd_ratio_J*(as_g_J_values/max_as_g_J_values)
+        max_a_g_J_unnorm_values = a_g_J_unnorm_values.max()
+        a_g_J_values_norm = a_g_J_unnorm_values/max_a_g_J_unnorm_values
+        a_g_J_values_scaled_to_max = a_g_peak_J*a_g_J_values_norm
 
-    ad_g_J_scaled_component = a_g_peak_J*ad_g_J_values/(max_ad_g_J_values*max_a_g_J_unnorm_values)
-    as_g_J_scaled_component = a_g_peak_J*a_sd_ratio_J*as_g_J_values/(max_as_g_J_values*max_a_g_J_unnorm_values)
+        ad_g_J_scaled_component = a_g_peak_J*ad_g_J_values/(max_ad_g_J_values*max_a_g_J_unnorm_values)
+        as_g_J_scaled_component = a_g_peak_J*a_sd_ratio_J*as_g_J_values/(max_as_g_J_values*max_a_g_J_unnorm_values)
 
-    # fitting
-    ad_params_f = curve_fit(a_g_comp, time_points, ad_g_J_scaled_component, [ad_tau_rise_J, ad_a1_J, ad_tau_dec_1_J, ad_a2_J, ad_tau_dec_2_J, ad_a3_J, ad_tau_dec_3_J])[0]
-    ad_g_fitted = partial(a_g_comp, tau_rise=ad_params_f[0], a1=ad_params_f[1], tau_dec_1=ad_params_f[2], a2=ad_params_f[3], tau_dec_2=ad_params_f[4], a3=ad_params_f[5], tau_dec_3=ad_params_f[6])
-    as_params_f = curve_fit(a_g_comp, time_points, as_g_J_scaled_component, [as_tau_rise_J, as_a1_J, as_tau_dec_1_J, as_a2_J, as_tau_dec_2_J, as_a3_J, as_tau_dec_3_J])[0]
-    as_g_fitted = partial(a_g_comp, tau_rise=as_params_f[0], a1=as_params_f[1], tau_dec_1=as_params_f[2], a2=as_params_f[3], tau_dec_2=as_params_f[4], a3=as_params_f[5], tau_dec_3=as_params_f[6])
+        # fitting
+        ad_params_f = curve_fit(a_g_comp, time_points, ad_g_J_scaled_component, [ad_tau_rise_J, ad_a1_J, ad_tau_dec_1_J, ad_a2_J, ad_tau_dec_2_J, ad_a3_J, ad_tau_dec_3_J])[0]
+        ad_g_fitted = partial(a_g_comp, tau_rise=ad_params_f[0], a1=ad_params_f[1], tau_dec_1=ad_params_f[2], a2=ad_params_f[3], tau_dec_2=ad_params_f[4], a3=ad_params_f[5], tau_dec_3=ad_params_f[6])
+        as_params_f = curve_fit(a_g_comp, time_points, as_g_J_scaled_component, [as_tau_rise_J, as_a1_J, as_tau_dec_1_J, as_a2_J, as_tau_dec_2_J, as_a3_J, as_tau_dec_3_J])[0]
+        as_g_fitted = partial(a_g_comp, tau_rise=as_params_f[0], a1=as_params_f[1], tau_dec_1=as_params_f[2], a2=as_params_f[3], tau_dec_2=as_params_f[4], a3=as_params_f[5], tau_dec_3=as_params_f[6])
 
-    def a_g(t):
-        return ad_g_fitted(t) + as_g_fitted(t)
+        def a_g(t):
+            return ad_g_fitted(t) + as_g_fitted(t)
 
-    # estimate error on waveform integral
-    a_g_values = np.array([a_g(t) for t in time_points])
-    integral_diff = np.abs(trapz(a_g_J_values_scaled_to_max - a_g_values, dx=0.001))
-    print(integral_diff)
+        # estimate error on waveform integral
+        a_g_values = np.array([a_g(t) for t in time_points])
+        integral_diff = np.abs(trapz(a_g_J_values_scaled_to_max - a_g_values, dx=0.001))
+        print(integral_diff)
 
-    param_file.write("AMPA direct parameters: {params}\n".format(params=zip(['tau_rise', 'a1', 'tau_dec_1', 'a2', 'tau_dec_2', 'a3', 'tau_dec_a3'], ad_params_f)))
-    param_file.write("AMPA spillover parameters: {params}\n".format(params=zip(['tau_rise', 'a1', 'tau_dec_1', 'a2', 'tau_dec_2', 'a3', 'tau_dec_a3'], as_params_f)))    
-    a_fig = plt.figure()
-    a_ax = a_fig.add_subplot(111)
-    a_ax.plot(time_points, a_g_J_values_scaled_to_max, label="Rothman 2009")
-    a_ax.plot(time_points, a_g_values, label="NeuroML")
-    a_ax.set_title('AMPA conductance waveform: direct+spillover')
-    a_ax.set_xlabel('time (ms)')
-    a_ax.set_ylabel('conductance (nS)')
-    a_ax.legend()
-    a_ax.grid()
-    
-#====NMDA====
-if nmda_fit:
-    voltage_points = np.arange(-80, -40, 0.001)
-    
-    n_g_unblock_J_values = np.array([n_g_unblock_J_unnorm(t, n_N_J, n_tau_rise_J, n_a1_J, n_tau_dec_1_J, n_a2_J, n_tau_dec_2_J) for t in time_points])
-    max_n_g_unblock_J_values = n_g_unblock_J_values.max()
-    n_g_unblock_J_norm_values = n_g_unblock_J_values/max_n_g_unblock_J_values
-    n_g_unblock_J_scaled = n_g_peak_J*n_g_unblock_J_norm_values
+        param_file.write("AMPA direct parameters: {params}\n".format(params=zip(['tau_rise', 'a1', 'tau_dec_1', 'a2', 'tau_dec_2', 'a3', 'tau_dec_a3'], ad_params_f)))
+        param_file.write("AMPA spillover parameters: {params}\n".format(params=zip(['tau_rise', 'a1', 'tau_dec_1', 'a2', 'tau_dec_2', 'a3', 'tau_dec_a3'], as_params_f)))
+        a_fig = plt.figure()
+        a_ax = a_fig.add_subplot(111)
+        a_ax.plot(time_points, a_g_J_values_scaled_to_max, label="Rothman 2009")
+        a_ax.plot(time_points, a_g_values, label="NeuroML")
+        a_ax.set_title('AMPA conductance waveform: direct+spillover')
+        a_ax.set_xlabel('time (ms)')
+        a_ax.set_ylabel('conductance (nS)')
+        a_ax.legend()
+        a_ax.grid()
 
-    n_block_eric_values = np.array([n_block_eric(v, n_K1slope_J, n_K2slope_J, n_VK1on_J, n_VK1off_J, n_VK2off_J) for v in voltage_points])
+    #====NMDA====
+    if nmda_fit:
+        voltage_points = np.arange(-80, -40, 0.001)
 
-    n_g_unblock_params_f = curve_fit(g_subcomp, time_points, n_g_unblock_J_scaled, [n_tau_rise_J, n_g_peak_J, (n_tau_dec_1_J*n_a1_J + n_tau_dec_2_J*n_a2_J)/(n_a1_J + n_a2_J)])[0]
-    n_block_params_f = curve_fit(n_block, voltage_points, n_block_eric_values, [1, 0.0035])[0]
-    
-    n_g_unblock_fitted = partial(g_subcomp, tau_rise=n_g_unblock_params_f[0], a=n_g_unblock_params_f[1], tau_dec=n_g_unblock_params_f[2])
-    n_block_fitted = partial(n_block, eta=n_block_params_f[0], gamma=n_block_params_f[1])
+        n_g_unblock_J_values = np.array([n_g_unblock_J_unnorm(t, n_N_J, n_tau_rise_J, n_a1_J, n_tau_dec_1_J, n_a2_J, n_tau_dec_2_J) for t in time_points])
+        max_n_g_unblock_J_values = n_g_unblock_J_values.max()
+        n_g_unblock_J_norm_values = n_g_unblock_J_values/max_n_g_unblock_J_values
+        n_g_unblock_J_scaled = n_g_peak_J*n_g_unblock_J_norm_values
 
-    n_g_unblock_values = np.array([n_g_unblock_fitted(t) for t in time_points])
-    n_block_values = np.array([n_block_fitted(v) for v in voltage_points])
+        n_block_eric_values = np.array([n_block_eric(v, n_K1slope_J, n_K2slope_J, n_VK1on_J, n_VK1off_J, n_VK2off_J) for v in voltage_points])
 
-    integral_diff = np.abs(trapz(n_g_unblock_J_scaled - n_g_unblock_values, dx=0.001))
-    print(integral_diff)
+        n_g_unblock_params_f = curve_fit(g_subcomp, time_points, n_g_unblock_J_scaled, [n_tau_rise_J, n_g_peak_J, (n_tau_dec_1_J*n_a1_J + n_tau_dec_2_J*n_a2_J)/(n_a1_J + n_a2_J)])[0]
+        n_block_params_f = curve_fit(n_block, voltage_points, n_block_eric_values, [1, 0.0035])[0]
+
+        n_g_unblock_fitted = partial(g_subcomp, tau_rise=n_g_unblock_params_f[0], a=n_g_unblock_params_f[1], tau_dec=n_g_unblock_params_f[2])
+        n_block_fitted = partial(n_block, eta=n_block_params_f[0], gamma=n_block_params_f[1])
+
+        n_g_unblock_values = np.array([n_g_unblock_fitted(t) for t in time_points])
+        n_block_values = np.array([n_block_fitted(v) for v in voltage_points])
+
+        integral_diff = np.abs(trapz(n_g_unblock_J_scaled - n_g_unblock_values, dx=0.001))
+        print(integral_diff)
 
 
-    param_file.write("NMDA (unbblocked) parameters: {params}\n".format(params=zip(['tau_rise', 'a', 'tau_dec'], n_g_unblock_params_f)))
-    param_file.write("NMDA block expression: {params}\n".format(params=zip(['eta', 'gamma'], n_block_params_f)))
+        param_file.write("NMDA (unbblocked) parameters: {params}\n".format(params=zip(['tau_rise', 'a', 'tau_dec'], n_g_unblock_params_f)))
+        param_file.write("NMDA block expression: {params}\n".format(params=zip(['eta', 'gamma'], n_block_params_f)))
 
-    n_fig = plt.figure()
-    n_ax = n_fig.add_subplot(111)
-    n_ax.plot(time_points, n_g_unblock_J_scaled, label="Jason's")
-    n_ax.plot(time_points, n_g_unblock_values, label="NeuroML")
-    n_ax.set_title('NMDA waveform (unblocked)')
-    n_ax.set_xlabel('time (ms)')
-    n_ax.set_ylabel('conductance (nS)')
-    n_ax.legend()
-    n_ax.grid()
+        n_fig = plt.figure()
+        n_ax = n_fig.add_subplot(111)
+        n_ax.plot(time_points, n_g_unblock_J_scaled, label="Jason's")
+        n_ax.plot(time_points, n_g_unblock_values, label="NeuroML")
+        n_ax.set_title('NMDA waveform (unblocked)')
+        n_ax.set_xlabel('time (ms)')
+        n_ax.set_ylabel('conductance (nS)')
+        n_ax.legend()
+        n_ax.grid()
 
-    nb_fig = plt.figure()
-    nb_ax = nb_fig.add_subplot(111)
-    nb_ax.plot(voltage_points, n_block_eric_values, label="Rothman 2009")
-    nb_ax.plot(voltage_points, n_block_values, label="NeuroML")
-    nb_ax.set_title('NMDA block')
-    nb_ax.set_xlabel('membrane potential (mV)')
-    nb_ax.set_ylabel('block factor')
-    nb_ax.legend()
-    nb_ax.grid()
+        nb_fig = plt.figure()
+        nb_ax = nb_fig.add_subplot(111)
+        nb_ax.plot(voltage_points, n_block_eric_values, label="Rothman 2009")
+        nb_ax.plot(voltage_points, n_block_values, label="NeuroML")
+        nb_ax.set_title('NMDA block')
+        nb_ax.set_xlabel('membrane potential (mV)')
+        nb_ax.set_ylabel('block factor')
+        nb_ax.legend()
+        nb_ax.grid()
 
-#===P7_9 blocking factor====
-if p7_9_fit:
+    #===P7_9 blocking factor====
+    if p7_9_fit:
 
-    lower_limit = -80
-    upper_limit = 0
-    plot_voltage_points = np.arange(-100, 40, 0.001) # plot region
-    voltage_points = np.arange(lower_limit, upper_limit, 0.001) # fit region
-    takahashi_block_values = np.array([block_takahashi_1996(v) for v in voltage_points])
+        lower_limit = -80
+        upper_limit = 0
+        plot_voltage_points = np.arange(-100, 40, 0.001) # plot region
+        voltage_points = np.arange(lower_limit, upper_limit, 0.001) # fit region
+        takahashi_block_values = np.array([block_takahashi_1996(v) for v in voltage_points])
 
-    p7_9_block_params_f = curve_fit(n_block, voltage_points, takahashi_block_values, [1, 0.0035])[0]
-    n_block_fitted = partial(n_block, eta=p7_9_block_params_f[0], gamma=p7_9_block_params_f[1])
-    n_block_values = np.array([n_block_fitted(v) for v in voltage_points])
+        p7_9_block_params_f = curve_fit(n_block, voltage_points, takahashi_block_values, [1, 0.0035])[0]
+        n_block_fitted = partial(n_block, eta=p7_9_block_params_f[0], gamma=p7_9_block_params_f[1])
+        n_block_values = np.array([n_block_fitted(v) for v in voltage_points])
 
-    p7_9_file = open('p7_9_fit.txt', 'w')
-    p7_9_file.write("NeuroMLv1 fit to Jason's Woodhull Fit of Takahashi 1996 results for p7-9:\n")
-    p7_9_file.write("Fitting region: from {lower}mV to {upper}mV\n".format(lower=lower_limit, upper=upper_limit))
-    p7_9_file.write("NMDA block expression: {params}\n".format(params=zip(['eta', 'gamma'], p7_9_block_params_f)))
-    p7_9_file.close()
+        p7_9_file = open('p7_9_fit.txt', 'w')
+        p7_9_file.write("NeuroMLv1 fit to Jason's Woodhull Fit of Takahashi 1996 results for p7-9:\n")
+        p7_9_file.write("Fitting region: from {lower}mV to {upper}mV\n".format(lower=lower_limit, upper=upper_limit))
+        p7_9_file.write("NMDA block expression: {params}\n".format(params=zip(['eta', 'gamma'], p7_9_block_params_f)))
+        p7_9_file.close()
 
-    p7_9_fig = plt.figure()
-    p7_9_ax = p7_9_fig.add_subplot(111)
-    p7_9_ax.plot(plot_voltage_points, [block_takahashi_1996(v) for v in plot_voltage_points], label="Jason's fit to Takahashi1996 p7-9")
-    p7_9_ax.plot(plot_voltage_points, [n_block_fitted(v) for v in plot_voltage_points], label="NeuroMLv1\n%s" % str(zip(['eta', 'gamma'], p7_9_block_params_f)))
-    p7_9_ax.set_title('NMDA block (fitted between %dmV and %dmV)' % (lower_limit, upper_limit))
-    p7_9_ax.set_xlabel('membrane potential (mV)')
-    p7_9_ax.set_ylabel('unblock factor')
-    p7_9_ax.legend(loc='upper left')
-    p7_9_ax.grid()
-    
-    
-    
-param_file.close()
-plt.show()
+        p7_9_fig = plt.figure()
+        p7_9_ax = p7_9_fig.add_subplot(111)
+        p7_9_ax.plot(plot_voltage_points, [block_takahashi_1996(v) for v in plot_voltage_points], label="Jason's fit to Takahashi1996 p7-9")
+        p7_9_ax.plot(plot_voltage_points, [n_block_fitted(v) for v in plot_voltage_points], label="NeuroMLv1\n%s" % str(zip(['eta', 'gamma'], p7_9_block_params_f)))
+        p7_9_ax.set_title('NMDA block (fitted between %dmV and %dmV)' % (lower_limit, upper_limit))
+        p7_9_ax.set_xlabel('membrane potential (mV)')
+        p7_9_ax.set_ylabel('unblock factor')
+        p7_9_ax.legend(loc='upper left')
+        p7_9_ax.grid()
+
+
+
+    param_file.close()
+    plt.show()
